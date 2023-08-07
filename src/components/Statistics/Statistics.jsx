bf1f1df30ee235f4f57f654634f7eb941211ee49
BozhiiKostya <kostyazap@gmail.com>
2023-08-07 13:39:41 +0300

    fixed bug

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,12 @@
 import { StatItemInPercent } from 'components/StatItemInPercent/StatItemInPercent';
 import { StatSaction, StatTitle, StatList } from './Statistics.styled';
 
-export const Statistics = ({ title, data }) => {
+export const Statistics = ({ title, stats }) => {
   return (
     <StatSaction className="statistics">
       <StatTitle className="title">{title}</StatTitle>
       <StatList className="stat-list">
-        <StatItemInPercent stats={data} />
+        <StatItemInPercent stats={stats} />
       </StatList>
     </StatSaction>
   );
